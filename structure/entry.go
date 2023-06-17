dd3d1b23d3487899ee7515064b420a3a4f98884a
Koen Bollen <meneer@koenbollen.nl>
2023-06-17 11:54:40 +0200

    Remove the full caps MESSAGE tag from Entry.

--- structure/entry.go
+++ structure/entry.go
@@ -8,7 +8,7 @@ type Entry struct {
 	RawTimestamp   string     `djson:"timestamp,@timestamp,time,date,ts"`
 	FloatTimestamp float64    `djson:"timestamp,@timestamp,time,date,ts"`
 	Severity       string     `djson:"severity,level,log.level"`
-	Message        string     `djson:"message,msg,text,MESSAGE"`
+	Message        string     `djson:"message,msg,text"`
 
 	Name string `djson:"app,name,service.name"`
 
